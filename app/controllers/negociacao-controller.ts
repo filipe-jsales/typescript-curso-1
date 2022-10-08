118c634a59d5cf2e9a0c32b80e7d7199c7cac52a
filipe_sales <eduardojs999@gmail.com>
2022-10-08 16:43:36 -0300

    ajustando visibilidade os métodos

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -17,26 +17,27 @@ export class NegociacaoController{
         this.negociacoesView.update(this.negociacoes);
     }
     
-    adiciona(): void{
+    public adiciona(): void{
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        
-        this.mensagemView.update('Cadastrado com sucesso!');
+        this.atualizaView();
         this.limparFormulario();
     }
-    criaNegociacao(): Negociacao{
+    public criaNegociacao(): Negociacao{
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ","));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value)
         return new Negociacao(date,quantidade,valor);
     }
-    limparFormulario(): void{
+    public limparFormulario(): void{
         this.inputData.value = "";
         this.inputQuantidade.value = "";
         this.inputValor.value = "";
         this.inputData.focus();
     }
-
+    private atualizaView(): void{
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Cadastrado com sucesso!');
+    }
 }
