9a782faa3f420112f2ab4ab1856ace6f2720b422
filipe_sales <eduardojs999@gmail.com>
2022-10-09 14:23:17 -0300

    método estático criaNegociacao

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -19,7 +19,11 @@ export class NegociacaoController{
     }
     
     public adiciona(): void{
-        const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criaNegociacao(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         //validando dia da semana 
         if (!this.isDiaUtil(negociacao.data)){
             this.mensagemView.update("Transações não são permitidas final de semana");
@@ -35,13 +39,7 @@ export class NegociacaoController{
         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
     }
 
-    public criaNegociacao(): Negociacao{
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ","));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value)
-        return new Negociacao(date,quantidade,valor);
-    }
+
     public limparFormulario(): void{
         this.inputData.value = "";
         this.inputQuantidade.value = "";
