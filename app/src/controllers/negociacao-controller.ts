560b165bd70ce6ddfd071772bf6c07178d83e5b9
filipe_sales <eduardojs999@gmail.com>
2022-10-11 10:44:16 -0300

    interface NegociacoesDodia

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -6,6 +6,7 @@ import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
+import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js';
 
 export class NegociacaoController {
     @domInjector('#data')
@@ -47,7 +48,7 @@ export class NegociacaoController {
     public importarDados(): void{
         fetch('http://localhost:8080/dados')
             .then(res => res.json())
-            .then((dados: any[]) => {
+            .then((dados: NegociacoesDoDia[]) => {
                 return dados.map(dado =>{
                     return new Negociacao(
                         new Date(), 
